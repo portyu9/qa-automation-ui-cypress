d2d385c662ff2d6f6a0716610f0effce144a4551
portyu9 <35150859+portyu9@users.noreply.github.com>
2025-09-03 19:21:23 -0700

    Create LoginPage.js

---
+++ cypress/pages/LoginPage.js
@@ -0,0 +1,45 @@
+/**
+ * Page Object for the Sauce Demo login page.  Encapsulates all selectors and
+ * page interactions so tests don’t have to know CSS details.  Using classes
+ * like this helps keep your tests expressive and easy to maintain.
+ */
+class LoginPage {
+  /** Navigate to the login page (root of the application). */
+  visit() {
+    cy.visit('/');
+  }
+
+  /** Username input element. */
+  get username() {
+    return cy.get('[data-test="username"]');
+  }
+
+  /** Password input element. */
+  get password() {
+    return cy.get('[data-test="password"]');
+  }
+
+  /** Login button element. */
+  get loginButton() {
+    return cy.get('[data-test="login-button"]');
+  }
+
+  /**
+   * Perform a login action.
+   *
+   * @param {string} username - The username to enter
+   * @param {string} password - The password to enter
+   */
+  login(username, password) {
+    this.username.clear().type(username);
+    this.password.clear().type(password);
+    this.loginButton.click();
+  }
+
+  /** Locator for the error message container. */
+  errorMessage() {
+    return cy.get('[data-test="error"]');
+  }
+}
+
+export default LoginPage;
