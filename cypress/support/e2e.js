7164a21542b2f52f93d74d77d8915fcbd35199b2
portyu9 <35150859+portyu9@users.noreply.github.com>
2025-09-03 19:32:38 -0700

    Create e2e.js

---
+++ cypress/support/e2e.js
@@ -0,0 +1,10 @@
+// ***********************************************************
+// This example support/e2e.js is processed and
+// loaded automatically before your test files.
+//
+// Use this file to import custom commands so that they are available
+// globally across all spec files.  See the documentation for more
+// details: https://docs.cypress.io/guides/core-concepts/writing-and-organizing-tests.
+// ***********************************************************
+
+import './commands';
